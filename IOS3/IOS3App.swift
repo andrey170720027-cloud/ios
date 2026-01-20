770f5177dca8480a7fd7dd4012e8cd612cc3f190
Andrey <andrey170720027@gmail.com>
2026-01-20 14:43:48 +0300

    Initial Commit

---
+++ IOS3/IOS3App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS3App.swift
+//  IOS3
+//
+//  Created by krupchakd on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct IOS3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
